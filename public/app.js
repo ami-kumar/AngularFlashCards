f73fbc28801eadff02195f5be0a7b3102e7e8c94
ami-kumar <ak3284@columbia.edu>
2015-04-06 15:28:29 -0400

    One answer and question showing up

--- public/app.js
+++ public/app.js
@@ -1,2 +1,15 @@
-var angular = require('angular')
 var app = angular.module('FlashCards', [])
+
+
+app.controller('FlashCardController', function( $scope ) {
+	// scope is javascript object both template and
+	// controller have access to
+	$scope.flashCard = {
+		question: 'What is Angular?',
+		answers: [
+			{ text: 'A front-end framework for great power!', correct: true },
+	        { text: 'Something lame, who cares, whatever.', correct: false },
+	        { text: 'Some kind of fish, right?', correct: false }
+		]
+	}
+})
